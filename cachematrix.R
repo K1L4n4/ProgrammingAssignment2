9955911068cc538c916ae267f6930c03b41a8b68
K1L4n4 <K1L4n4@users.noreply.github.com>
2015-07-20 15:16:05 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,5 @@
 ## The assignment is to write a pair of functions that cache the inverse of a matrix:
-##      makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
-##      cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-##      If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should 
-##      retrieve the inverse from the cache.
+##      makeCacheMatrix and cacheSolve.
 
 
 ## makeCacheMatrix: This function creates a special "matrix" object 
